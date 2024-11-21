4c4424f46276350eaa7b388aa9942584bb0279b2
Mehdy-H-Asaad <mehdyasaad.sy.2003@gmail.com>
2024-11-21 21:58:53 +0400

    FIXED

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ReactQueryProvider } from "./services/providers/ReactQueryProvider.tsx";
 import ScrollToTop from "./components/ScrollTo.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<ScrollToTop />
 			<ReactQueryProvider>
 				<App />
 			</ReactQueryProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
